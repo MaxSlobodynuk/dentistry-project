06924b871ab3b3703f5eb0199c05158be0a2eab5
MaxSlobodynuk <119804658+MaxSlobodynuk@users.noreply.github.com>
2024-02-07 20:20:32 +0000

    Deploying to gh-pages from @ MaxSlobodynuk/dentistry-project@3125a6df481a7f45038076ed69d69b23112812a1 🚀

---
+++ index.6f51e291.js
@@ -0,0 +1,2 @@
+fetch("https://api.demo.edenlab.com.ua").then((o=>{if(!o.ok)throw new Error(o.statusText);return o.json()})).then((o=>console.log(o))).catch((o=>console.log(o)));
+//# sourceMappingURL=index.6f51e291.js.map
