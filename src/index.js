4438bec7621d31f9ed4c8bae32be76939eaa7fa4
Max <slobodynuk.777@gmail.com>
2024-02-09 16:09:43 +0200

    add two sections

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function getInformation() {
-    return fetch('https://api.demo.edenlab.com.ua')
-    .then((resp) => {
-        if (!resp.ok) {
-            throw new Error(resp.statusText);
-        }
 
-        return resp.json();
-    })
-}
 
-getInformation().then(data => console.log(data)).catch(err => console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import './js/markup/services-markup';
