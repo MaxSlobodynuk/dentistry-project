bb462aad4b0ac1b08342283cbb5623d17f8b84e8
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-02-09 09:06:27 -0500

    removed the comment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-// function getInformation() {
-//     return fetch('https://api.dentally.co')
-//     .then((resp) => {
-//         if (!resp.ok) {
-//             throw new Error(resp.statusText);
-//         }
+function getInformation() {
+    return fetch('https://api.dentally.co')
+    .then((resp) => {
+        if (!resp.ok) {
+            throw new Error(resp.statusText);
+        }
 
-//         return resp.json();
-//     })
-// }
+        return resp.json();
+    })
+}
 
-// getInformation().then(data => console.log(data)).catch(err => console.log(err))
+getInformation().then(data => console.log(data)).catch(err => console.log(err))
