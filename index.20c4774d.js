977dbe107aa2323a7ea22633d5759e77b6678bc7
MaxSlobodynuk <119804658+MaxSlobodynuk@users.noreply.github.com>
2024-02-09 14:27:25 +0000

    Deploying to gh-pages from @ MaxSlobodynuk/dentistry-project@4438bec7621d31f9ed4c8bae32be76939eaa7fa4 🚀

--- index.20c4774d.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.demo.edenlab.com.ua").then((function(n){if(!n.ok)throw new Error(n.statusText);return n.json()})).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}));
-//# sourceMappingURL=index.20c4774d.js.map
--- index.6f51e291.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.demo.edenlab.com.ua").then((o=>{if(!o.ok)throw new Error(o.statusText);return o.json()})).then((o=>console.log(o))).catch((o=>console.log(o)));
-//# sourceMappingURL=index.6f51e291.js.map
