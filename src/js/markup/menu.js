7c690e0091c6705d4b1606a99a653f2fbef0460c
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-03-06 22:09:21 -0500

    fixed menu

--- src/js/markup/menu.js
+++ src/js/markup/menu.js
@@ -1,11 +1,15 @@
 const mobMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector(".menu-btn");
 const openMenu = document.querySelector('.js-open-menu');
+const noScroll = document.querySelector('body')
 
 openMenu.addEventListener('click', ()=> {
 mobMenu.hidden = false;
+noScroll.classList.add('no-scroll');
 })
 
 closeMenu.addEventListener('click', ()=>{
     mobMenu.hidden = true;
+    noScroll.classList.remove('no-scroll');
 })
+
