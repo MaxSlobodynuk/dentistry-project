b7f7eea4f2f904eea589d99b1636882cc007d80d
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-03-10 17:31:59 -0400

    add options to menu

--- src/js/markup/menu.js
+++ src/js/markup/menu.js
@@ -1,15 +1,30 @@
 const mobMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector(".menu-btn");
 const openMenu = document.querySelector('.js-open-menu');
-const noScroll = document.querySelector('body')
+const noScroll = document.querySelector('body');
 
-openMenu.addEventListener('click', ()=> {
-mobMenu.hidden = false;
-noScroll.classList.add('no-scroll');
-})
 
-closeMenu.addEventListener('click', ()=>{
-    mobMenu.hidden = true;
+openMenu.addEventListener('click', onOpen);
+closeMenu.addEventListener('click', onClose);
+
+function onOpen(){
+    window.addEventListener('keydown', keyEsc);
+    mobMenu.classList.remove("is-hidden");
+    noScroll.classList.add('no-scroll');
+    mobMenu.classList.add('is-transformMenu');
+}
+
+function onClose(){
+    window.removeEventListener('keydown', keyEsc);
+    mobMenu.classList.add("is-hidden");
     noScroll.classList.remove('no-scroll');
-})
+    mobMenu.classList.remove('is-transformMenu');
+}
 
+
+function keyEsc (e){
+    const ESC = "Escape";
+    if(e.code === ESC ){
+        onClose();
+    }
+}
